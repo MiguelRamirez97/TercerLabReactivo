05a1cd300568da5167469893b755068349b19ab5
Miguel <miguel.ramirez@udea.edu.co>
2021-05-19 22:14:54 -0500

    correcciones de algunos metodos con mejor refactorizacion

--- src/main/java/com/example/HelperKata.java
+++ src/main/java/com/example/HelperKata.java
@@ -11,7 +11,6 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
@@ -20,23 +19,44 @@ public class HelperKata {
     private static String anteriorBono = null;
 
     public static Flux<CouponDetailDto> getListFromBase64File(final String fileBase64) {
-        return getCouponDetailDtoFlux(createFluxFrom(fileBase64));
-    }
-
-    private static Flux<CouponDetailDto> getCouponDetailDtoFlux(Flux<String> fileBase64) {
         AtomicInteger counter = new AtomicInteger(0);
         String characterSeparated = FileCSVEnum.CHARACTER_DEFAULT.getId();
         Set<String> codes = new HashSet<>();
-        return fileBase64.skip(1)
-                    .map(line -> getTupleOfLine(line, line.split(characterSeparated), characterSeparated))
-                    .map(tuple -> getCouponDetailDto(counter, codes, tuple));
+        return createFluxFrom(fileBase64).skip(1)
+                .map(line -> getTupleOfLine(line, line.split(characterSeparated), characterSeparated))
+                .map(tuple -> getCouponDetailDto(counter, codes, tuple));
+    }
+
+    private static Flux<String> createFluxFrom(String fileBase64) {
+        return Flux.using(() -> new BufferedReader(new InputStreamReader(
+                        new ByteArrayInputStream(decodeBase64(fileBase64))
+                )).lines(),
+                Flux::fromStream,
+                Stream::close
+        );
+    }
+
+    private static Tuple2<String, String> getTupleOfLine(String line, String[] array, String characterSeparated) {
+        Tuple2<String, String> tuple = Tuples.of(array[0], array[1]);
+        if(nullOrEmpty(array)){
+            tuple = Tuples.of(EMPTY_STRING, EMPTY_STRING);
+        }else if (array.length < 2){
+            tuple = line.startsWith(characterSeparated) ?
+                    Tuples.of(EMPTY_STRING, array[0]) : Tuples.of(array[0], EMPTY_STRING);
+        }
+        return tuple;
+    }
+
+    public static boolean nullOrEmpty(String[] array){
+        return Objects.isNull(array) || array.length == 0;
     }
 
     private static CouponDetailDto getCouponDetailDto(AtomicInteger counter, Set<String> codes, Tuple2<String, String> tuple) {
         String dateValidated = null;
         String errorMessage;
-        String bonoForObject;
         String bonoEnviado;
+        String bonoForObject;
+
 
         errorMessage = getErrorMessage(tuple, codes);
         if (errorMessage == null) {
@@ -55,14 +75,7 @@ public class HelperKata {
                 .build();
     }
 
-    private static Flux<String> createFluxFrom(String fileBase64) {
-        return Flux.using(() -> new BufferedReader(new InputStreamReader(
-                new ByteArrayInputStream(decodeBase64(fileBase64))
-                )).lines(),
-                Flux::fromStream,
-                Stream::close
-        );
-    }
+
 
     public static String typeBono(String bonoIn) {
         String typeBono;
@@ -79,30 +92,35 @@ public class HelperKata {
     }
 
     public static Boolean bonoEAN13(String bonoIn){
-        return bonoIn.chars().allMatch(Character::isDigit) && bonoIn.length() >= 12 && bonoIn.length() <= 13;
+        return bonoIn.chars().allMatch(Character::isDigit) && length12or13(bonoIn);
+    }
+
+    public static Boolean length12or13(String bonoIn){
+        return bonoIn.length() >= 12 && bonoIn.length() <= 13;
     }
 
     public static Boolean bonoEAN39(String bonoIn){
-        return bonoIn.startsWith("*") && bonoIn.replace("*", "").length() >= 1
+        return bonoIn.startsWith("*") && bonoReplace(bonoIn);
+    }
+
+    public static Boolean bonoReplace(String bonoIn){
+        return bonoIn.replace("*", "").length() >= 1
                 && bonoIn.replace("*", "").length() <= 43;
     }
 
     public static String getErrorMessage(Tuple2<String,String> tuple,Set<String> codes){
-        String errorMessage;
+        String errorMessage = null;
         if (errorColumnEmpty(tuple)) {
             errorMessage = ExperienceErrorsEnum.FILE_ERROR_COLUMN_EMPTY.toString();
         }
-        else if (!codes.add(tuple.getT1())) {
+        if (!codes.add(tuple.getT1())) {
             errorMessage = ExperienceErrorsEnum.FILE_ERROR_CODE_DUPLICATE.toString();
         }
-        else if (!validateDateRegex(tuple.getT2())) {
+        if (!validateDateRegex(tuple.getT2())) {
             errorMessage = ExperienceErrorsEnum.FILE_ERROR_DATE_PARSE.toString();
         }
-        else if (validateDateIsMinor(tuple.getT2())) {
+        if (validateDateIsMinor(tuple.getT2())) {
             errorMessage = ExperienceErrorsEnum.FILE_DATE_IS_MINOR_OR_EQUALS.toString();
-        }
-        else {
-            errorMessage = null;
         }
         return errorMessage;
     }
@@ -123,16 +141,6 @@ public class HelperKata {
 
     }
 
-    private static Tuple2<String, String> getTupleOfLine(String line, String[] array, String characterSeparated) {
-        return Objects.isNull(array) || array.length == 0
-                ? Tuples.of(EMPTY_STRING, EMPTY_STRING)
-                : array.length < 2
-                ? line.startsWith(characterSeparated)
-                ? Tuples.of(EMPTY_STRING, array[0])
-                : Tuples.of(array[0], EMPTY_STRING)
-                : Tuples.of(array[0], array[1]);
-    }
-
     public static boolean validateDateIsMinor(String dateForValidate) {
         try {
             SimpleDateFormat sdf = new SimpleDateFormat(FileCSVEnum.PATTERN_SIMPLE_DATE_FORMAT.getId());
@@ -150,10 +158,9 @@ public class HelperKata {
         if (bonoAnteriorNullOrEmpty()) {
             anteriorBono = typeBono(bonoEnviado);
             bonoForObject = anteriorBono.equals("") ? null : bonoEnviado;
-        } else if (anteriorBono.equals(typeBono(bonoEnviado))) {
+        }
+        if (anteriorBono.equals(typeBono(bonoEnviado))) {
             bonoForObject = bonoEnviado;
-        } else if (!anteriorBono.equals(typeBono(bonoEnviado))) {
-            bonoForObject = null;
         }
         return bonoForObject;
     }
